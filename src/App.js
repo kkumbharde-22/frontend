ed2862019738a58f1676fe6147a4c37b7facd155
Sneha14-bit <snehatripathi14st@gmail.com>
2024-02-16 20:04:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home";
+import Legal from "./Pages/Legal";
+import NotFound from "./Pages/NotFound";
+import Appointment from "./Pages/Appointment";
+import Login from "./Components/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <Router basename="/Health-Plus">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/legal" element={<Legal />} />
+          <Route path="/appointment" element={<Appointment />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
