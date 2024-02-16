05b56923b64da48e462facc7830435e2268a1334
Sneha14-bit <snehatripathi14st@gmail.com>
2024-02-16 22:40:05 +0530

    login.js file modified

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,124 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "../Styles/AppointmentForm.css";
+import { ToastContainer, toast } from "react-toastify";
 
-const Login = () => {
+function Login() {
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+
+  const [patientName, setPatientName] = useState("");
+  const [patientNumber, setPatientNumber] = useState("");
+  const [patientGender, setPatientGender] = useState("default");
+  const [appointmentTime, setAppointmentTime] = useState("");
+  const [preferredMode, setPreferredMode] = useState("default");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [formErrors, setFormErrors] = useState({});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validate form inputs
+    const errors = {};
+    if (!patientName.trim()) {
+      errors.patientName = "Patient name is required";
+    }
+
+    if (!patientNumber.trim()) {
+      errors.patientNumber = "Patient phone number is required";
+    } else if (patientNumber.trim().length !== 10) {
+      errors.patientNumber = "Patient phone number must be of 10 digits";
+    }
+
+    if (patientGender === "default") {
+      errors.patientGender = "Please select patient gender";
+    }
+
+    if (Object.keys(errors).length > 0) {
+      setFormErrors(errors);
+      return;
+    }
+
+    // Reset form fields and errors after successful submission
+    setPatientName("");
+    setPatientNumber("");
+    setPatientGender("default");
+    setAppointmentTime("");
+    setPreferredMode("default");
+    setFormErrors({});
+
+    toast.success("Appointment Scheduled !", {
+      position: toast.POSITION.TOP_CENTER,
+      onOpen: () => setIsSubmitted(true),
+      onClose: () => setIsSubmitted(false),
+    });
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div className="appointment-form-section">
+
+      <div className="form-container">
+        <h2 className="form-title">
+          <span>Login</span>
+        </h2>
+
+        <form className="form-content" onSubmit={handleSubmit}>
+          <label>
+            Username:
+            <input
+              type="text"
+              value={patientName}
+              onChange={(e) => setPatientName(e.target.value)}
+              required
+            />
+            {formErrors.patientName && <p className="error-message">{formErrors.patientName}</p>}
+          </label>
+
+          <br />
+          <label>
+            Patient Phone Number:
+            <input
+              type="text"
+              value={patientNumber}
+              onChange={(e) => setPatientNumber(e.target.value)}
+              required
+            />
+            {formErrors.patientNumber && <p className="error-message">{formErrors.patientNumber}</p>}
+          </label>
+
+          <br />
+          <label>
+            Patient Gender:
+            <select
+              value={patientGender}
+              onChange={(e) => setPatientGender(e.target.value)}
+              required
+            >
+              <option value="default">Select</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="private">I will inform Doctor only</option>
+            </select>
+            {formErrors.patientGender && <p className="error-message">{formErrors.patientGender}</p>}
+          </label>
+
+          <br />
+          <button type="submit" className="text-registration-btn">
+            Confirm Registration
+          </button>
+
+          <p className="success-message" style={{display: isSubmitted ? "block" : "none"}}>Appointment details has been sent to the patients phone number via SMS.</p>
+        </form>
+      </div>
+
+      <div className="legal-footer">
+        <p>© 2013-2023 Health+. All rights reserved.</p>
+      </div>
+
+      <ToastContainer autoClose={5000} limit={1} closeButton={false} />
+    </div>
+  );
 }
 
-export default Login
+export default Login;
